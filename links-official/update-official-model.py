aab3bec302e71c2e1984e50cdc09811008f6ce0c
ximenes <xresende@gmail.com>
2016-11-30 13:38:26 -0200

    add magnet profile to link-official folder

--- links-official/update-official-model.py
+++ links-official/update-official-model.py
@@ -102,6 +102,7 @@ def copy_files(r):
     os.system('cp -raf ../' + r['magnet_model_version'] + '/analysis/fieldmap/' + r['fmap_analysis_folder'] + '/field_on_trajectory.txt ./')
     os.system('cp -raf ../' + r['magnet_model_version'] + '/analysis/fieldmap/' + r['fmap_analysis_folder'] + '/multipoles.txt ./')
     os.system('cp -raf ../' + r['magnet_model_version'] + '/analysis/fieldmap/' + r['fmap_analysis_folder'] + '/trajectory.txt ./')
+    os.system('cp -raf ../' + r['magnet_model_version'] + '/analysis/fieldmap/' + r['fmap_analysis_folder'] + '/*.svg ./')
 
 def main(argv):
     print('selecting analysed rawfield file...')
