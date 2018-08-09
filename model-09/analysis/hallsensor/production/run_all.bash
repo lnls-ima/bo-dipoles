0efd4a1d911d159536d41b60571fea3ebba8df07
ximenes <xresende@gmail.com>
2018-08-09 14:14:53 -0300

    Updating files

--- model-09/analysis/hallsensor/production/run_all.bash
+++ model-09/analysis/hallsensor/production/run_all.bash
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+#cd x0-9p1013mm/
+cd x0-9p0450mm-old/
+
 for ima in $(ls | grep bd); do
     for fol in positive negative; do
         cd $ima/M1/0991p63A/z-$fol/ && \
@@ -8,3 +11,12 @@ for ima in $(ls | grep bd); do
         cd ../../../../;
     done
 done
+
+# for ima in $(ls | grep bd); do
+#     for fol in positive negative; do
+#         cd $ima/M1/0991p63A/z-$fol/ && \
+#         echo running magnet $ima in the $fol direction && \
+#         fac-fma-analysis.py clean && \
+#         cd ../../../../;
+#     done
+# done
