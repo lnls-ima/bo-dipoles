e327e7d3238f3226ff806eccbeb05d312d94b305
ximenes <xresende@gmail.com>
2018-08-10 08:27:22 -0300

    Reorganizing analysis files

---
+++ model-09/analysis/hallprobe/excitation_curve/run_all.bash
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# cd bd-042/0040p31A/z-negative; fac-fma-analysis.py run; cd ../../../
+# cd bd-042/0040p31A/z-positive; fac-fma-analysis.py run; cd ../../../
+# cd bd-042/0050p39A/z-negative; fac-fma-analysis.py run; cd ../../../
+# cd bd-042/0050p39A/z-positive; fac-fma-analysis.py run; cd ../../../
+# cd bd-042/0060p46A/z-negative; fac-fma-analysis.py run; cd ../../../
+# cd bd-042/0060p46A/z-positive; fac-fma-analysis.py run; cd ../../../
+# cd bd-042/0165p27A/z-negative; fac-fma-analysis.py run; cd ../../../
+# cd bd-042/0165p27A/z-positive; fac-fma-analysis.py run; cd ../../../
+# cd bd-042/0330p54A/z-negative; fac-fma-analysis.py run; cd ../../../
+# cd bd-042/0330p54A/z-positive; fac-fma-analysis.py run; cd ../../../
+# cd bd-042/0942p05A/z-negative; fac-fma-analysis.py run; cd ../../../
+# cd bd-042/0942p05A/z-positive; fac-fma-analysis.py run; cd ../../../
+# cd bd-042/0991p63A/z-negative; fac-fma-analysis.py run; cd ../../../
+# cd bd-042/0991p63A/z-positive; fac-fma-analysis.py run; cd ../../../
+# cd bd-042/1041p21A/z-negative; fac-fma-analysis.py run; cd ../../../
+# cd bd-042/1041p21A/z-positive; fac-fma-analysis.py run; cd ../../../
+
+
+# cd bd-016/0040p31A/z-negative; fac-fma-analysis.py clean; cd ../../../
+# cd bd-016/0040p31A/z-positive; fac-fma-analysis.py clean; cd ../../../
+
+cd x0-9p0450mm-old
+
+cd bd-016/0050p39A/z-negative; fac-fma-analysis.py run; cd ../../../
+cd bd-016/0050p39A/z-positive; fac-fma-analysis.py run; cd ../../../
+cd bd-016/0991p63A/z-negative; fac-fma-analysis.py run; cd ../../../
+cd bd-016/0991p63A/z-positive; fac-fma-analysis.py run; cd ../../../
+cd bd-027/0040p31A/z-negative; fac-fma-analysis.py run; cd ../../../
+cd bd-027/0040p31A/z-positive; fac-fma-analysis.py run; cd ../../../
+cd bd-027/0050p39A/z-negative; fac-fma-analysis.py run; cd ../../../
+cd bd-027/0050p39A/z-positive; fac-fma-analysis.py run; cd ../../../
+cd bd-027/0991p63A/z-negative; fac-fma-analysis.py run; cd ../../../
+cd bd-027/0991p63A/z-positive; fac-fma-analysis.py run; cd ../../../
